20111860d649e146fb8d43a128c67a7d4c567cf7
stella2669 <stella2669@gmail.com>
2020-11-25 15:04:23 +0900

    Array.length

---
+++ chapter05/ex_05_02.js
@@ -0,0 +1,5 @@
+var varArr = new Array(0,1,2,3,4,5);
+console.log("varArr.length : " + varArr.length); // 6
+
+var varArrTemp = new Array();
+console.log("varArrTemp.length : " + varArrTemp.length); // 0
