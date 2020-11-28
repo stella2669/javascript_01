0270aba5eba7e9a8da741c5e02004558e66a1ded
stella2669 <stella2669@gmail.com>
2020-11-28 15:33:07 +0900

    if문 else if문

---
+++ chapter06/ex_06_01.js
@@ -0,0 +1,25 @@
+var varNum = 100;
+
+if(varNum > 50){
+  console.log("varNum > 50"); // 출력
+}
+if(varNum <= 50){
+  console.log("varNum <= 50");
+}
+console.log("varNum : " + varNum);
+
+if(varNum < 50){
+  console.log("varNum < 50");
+}else{
+  console.log("varNum >= 50"); // 출력
+}
+
+if(varNum > 200){
+  console.log("varNum > 200");
+}else if(varNum > 150){
+  console.log("varNUm > 150")
+}else if(varNum > 100){
+  console.log("varNum > 100");
+}else{
+  console.log("varNum <= 100"); // 출력
+}
